6dd65141a39a532ce123b3bcb8114d9069ea8aaf
yangchichome <118092513+yangchichome@users.noreply.github.com>
2023-06-17 10:13:18 +0800

    'AngularExample'

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { Student } from '../../shared/student.model';
+
+@Component({
+  // select: '[app-server]',
+  selector: 'app-server',
+  templateUrl: './server.component.html', // you can put real html here
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent {
+    serverId = 10;
+    serverStatus = 'online';
+    counter = 0;
+    isLogin = false;
+    userName: string = '';
+    students: Student[] = [new Student('Bill Gates', 'Computer Science'),
+                           new Student('Steve Jobs', 'Computer Science'),
+                           new Student('Elon Musk', 'Computer Science')];
+
+    getServerStatus() {
+      return this.serverStatus;
+    }
+
+    counterPlus() {
+     this.counter ++;
+    }
+
+    resetCounter() {
+      this.counter = 0;
+    }
+
+    login() {
+      this.isLogin = true;
+    }
+
+    signOut() {
+      this.isLogin = false;
+    }
+
+    // Event Binding
+    onUpdateUserName(event: Event) {
+      console.log(event);
+      this.userName = (<HTMLInputElement>event.target).value;
+    }
+}
